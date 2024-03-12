c36a419e56d11a8286ab6d7f7ea80cb9305c6b75
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2024-03-12 16:28:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,24 +5,34 @@ let gettingUser=document.getElementById("getUser");
 let image =document.querySelector(".img");
 let description =document.querySelector(".description");
 let name =document.querySelector(".name");
-
+let age=document.getElementById("ageBtn");
+let email=document.getElementById("emailBtn");
+let phone=document.getElementById("phoneBtn");
 let userData;
 async function fetchUser(){
     const res=await fetch("https://randomuser.me/api/");
 	const data=await res.json();
 	// console.log(data)
 	userData=data.results[0];
+	image.src=`${userData.picture.medium}`;
+	name.innerText=`${userData.name.first} ${userData.name.last}`
+	description.innerText="";
 	console.log(userData);
 }
 fetchUser();
 gettingUser.addEventListener("click", ()=>{
 	fetchUser();
-	image.src=`${userData.picture.medium}`;
-	name.innerText=`${userData.name.first} ${userData.name.last}`
 })
-
-	image.src=`${userData.picture.medium}`;
-	name.innerText=`${userData.name.first} ${userData.name.last}`
+age.addEventListener("click", ()=>{
+	description.innerText=`${userData.dob.age}`;
+})
+email.addEventListener("click", ()=>{
+	description.innerText=`${userData.email}`;
+})
+phone.addEventListener("click", ()=>{
+	description.innerText=`${userData.phone}`;
+})
+	
 
 
 
