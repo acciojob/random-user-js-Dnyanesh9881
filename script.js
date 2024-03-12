d9b7fd15a21055856195ccf0fe37260ca6efdf6b
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2024-03-12 16:13:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,16 +2,27 @@
 
 let user =document.querySelector(".user");
 let gettingUser=document.getElementById("getUser");
-let userData=[];
+let image =document.querySelector(".img");
+let description =document.querySelector(".description");
+let name =document.querySelector(".name");
+
+let userData;
 async function fetchUser(){
-    let res=await fetch("https://randomuser.me/api/");
-	let data=await res.json();
+    const res=await fetch("https://randomuser.me/api/");
+	const data=await res.json();
 	// console.log(data)
-	userData=data.results;
+	userData=data.results[0];
 	console.log(userData);
 }
 fetchUser();
 gettingUser.addEventListener("click", ()=>{
 	fetchUser();
 })
+if(userData){
+	image.src=`${userData.picture.medium}`;
+	name.innerText=`${userData.name.first} ${userData.name.last}`
+}
+
+
+
 
