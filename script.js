30d91a138af551a69aafa7dcf140f5f3ad321940
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2024-03-12 15:42:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your code here
+
+let user =document.querySelector(".user");
+let gettingUser=document.getElementById("getUser");
+let userData=[];
+async function fetchUser(){
+    let res=await fetch("https://randomuser.me/api/");
+	let data=await res.json();
+	// console.log(data)
+	userData=data.results;
+	console.log(userData);
+}
+fetchUser();
+gettingUser.addEventListener("click", ()=>{
+	fetchUser();
+})
+
